8103797fb473bffdfebb68aac6eff7811489acb6
unmen <unmensajeparami@gmail.com>
2023-01-12 23:12:37 +0100

    Segundo commit en Eclipse.

--- eclipse-workspace/Practica3.4/src/Clase.java
+++ eclipse-workspace/Practica3.4/src/Clase.java
@@ -5,12 +5,7 @@ public class Clase {
 	private Integer platos, pinones;
 	private Float precio;
 	
-	public Clase (String modelo, String ruedas, Integer platos, Integer pinones, Float precio){
-		this.modelo = modelo;
-		this.ruedas = ruedas;
-		this.platos = platos;
-		this.pinones = pinones;
-		this.precio = precio;
-	}
+	
+	
 	
 }
