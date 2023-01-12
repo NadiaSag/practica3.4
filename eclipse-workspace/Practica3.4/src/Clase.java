326cdc7d13ee10bd53a641c581c83a75fc147914
unmen <unmensajeparami@gmail.com>
2023-01-12 23:10:58 +0100

    Primer commit en Eclipse.

---
+++ eclipse-workspace/Practica3.4/src/Clase.java
@@ -0,0 +1,16 @@
+
+public class Clase {
+	
+	private String modelo = "", ruedas = "";
+	private Integer platos, pinones;
+	private Float precio;
+	
+	public Clase (String modelo, String ruedas, Integer platos, Integer pinones, Float precio){
+		this.modelo = modelo;
+		this.ruedas = ruedas;
+		this.platos = platos;
+		this.pinones = pinones;
+		this.precio = precio;
+	}
+	
+}
